f523ca00231ca5461ef06b9a447345e7b771566f
Clement Barbaza <clement.barbaza@gmail.com>
2021-06-27 08:38:12 +0200

    update schema sql

--- schema.sql
+++ schema.sql
@@ -21,6 +21,8 @@ CREATE TABLE public.todos (
     id integer DEFAULT nextval('public.todos_id_seq'::regclass) NOT NULL,
     username character varying(255) NOT NULL,
     body text NOT NULL,
-    attachments text,
-    todo_id integer NOT NULL
+    images text,
+    todo_id integer NOT NULL,
+    videos text,
+    created_at timestamp(0) without time zone DEFAULT now()
 );
