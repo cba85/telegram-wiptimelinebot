36d34ca3fbf23fce17a37f9f99480db2e47459c4
Clément Barbaza <clement.barbaza@gmail.com>
2023-04-29 10:13:14 +0200

    secure app

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const compression = require("compression");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 const Telegram = require("./src/telegram");
 const Db = require("./src/db/db");
 
 (async () => {
   const app = express();
+
   app.use(bodyParser.json());
+  app.use(compression());
+  app.use(helmet());
+
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  });
+
+  app.use(limiter);
 
   const db = await new Db();
 
