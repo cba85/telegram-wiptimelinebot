5e7228fc0eea86fbfca8fff2e17f9cf3a09c0995
Clément Barbaza <clement.barbaza@gmail.com>
2023-05-14 12:52:42 +0200

    change user schema mysql db

--- db/mysql.sql
+++ db/mysql.sql
@@ -17,10 +17,10 @@ CREATE TABLE todos (
 
 CREATE TABLE users (
     id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username varchar(255) NOT NULL,
+    username varchar(255),
     first_name varchar(255),
     last_name varchar(255),
-    is_bot tinyint NOT NULL,
+    is_bot tinyint,
     language_code varchar(255),
     date timestamp NOT NULL DEFAULT now()
 );
