d47064ecb30b453cf83f58ffbecc22d923fa964d
Clément Barbaza <clement.barbaza@gmail.com>
2023-10-29 20:44:23 +0100

    remove rate limiter

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
 const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
 const Telegram = require("./src/telegram");
 const Db = require("./src/db/db");
 const nunjucks = require("nunjucks");
@@ -15,15 +14,6 @@ const nunjucks = require("nunjucks");
   app.use(bodyParser.json());
   app.use(compression());
   app.use(helmet());
-
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  });
-
-  app.use(limiter);
 
   let noCache = false;
   if (process.env.APP_ENV == "local") {
